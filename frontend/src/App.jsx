e1e87210f804cb296276d6ea5c5cfc70357930e6
adithyanajay <adithyanajay@gmail.com>
2026-01-09 23:34:33 +0530

    inital commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div>
+      <h1 className="text-5xl text-center mtop-100">Hello, Treesa</h1>
+      <h1 className="text-2xl text-center mtop-100">Stay cool 😎</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
