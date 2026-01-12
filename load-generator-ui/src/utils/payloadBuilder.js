293d3332a6b2017bfcd7f5d0fc58aba93a6529e4
Treesa George <treesageorge76@gmail.com>
2026-01-13 01:01:22 +0530

    .update

--- load-generator-ui/src/utils/payloadBuilder.js
+++
@@ -1,9 +0,0 @@
-export const buildPayload = (type, target, duration, parameters) => {
-  return {
-    type,
-    target,
-    duration_seconds: duration,
-    mode: "constant",
-    parameters,
-  };
-};
